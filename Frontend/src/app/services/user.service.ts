813dab88eb39c5ae27cffc47c55259cd354d2378
vitorrf4 <vitorrf4@gmail.com>
2023-09-24 10:34:00 -0300

    write basic error handling for user frontend service

--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -1,33 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from "../models/user";
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable()
 export class UserService {
-  private readonly userUrl: string;
-  public usersListBehaviorSubject = new BehaviorSubject<User[]>([]);
+  private readonly userUrl: string = 'http://localhost:8080/users';
+  public users = new BehaviorSubject<User[]>([]);
 
-  constructor(private http: HttpClient) {
-    this.userUrl = "http://localhost:8080/users";
-  }
+  constructor(private http: HttpClient) { }
 
-  public getList(): BehaviorSubject<User[]> {
-    this.http.get<User[]>(`${this.userUrl}`).subscribe(
-      data => this.usersListBehaviorSubject.next(data));
-    return this.usersListBehaviorSubject;
-  }
+    public getList(): BehaviorSubject<User[]> {
+        this.http.get<User[]>(this.userUrl).subscribe({
+            next: res => this.users.next(res),
+            error : err => alert(err.message)
+        });
 
-	public addUser(user : User) {
-    this.http.post(`${this.userUrl}`, user).subscribe(data => {
-      this.usersListBehaviorSubject.getValue().push(new User(data));
-    });
+        return this.users;
+    }
+
+    public addUser(user : User) {
+        this.http.post(this.userUrl, user).subscribe({
+            next: res=> this.users.getValue().push(new User(res)),
+            error: err => alert(err.message)
+        });
 	}
 
-	public deleteUser(id : Number){
-    this.http.delete(`${this.userUrl}/${id}`).subscribe(data => {
-      const listWithoutDeletedUser = this.usersListBehaviorSubject.value.filter(u => u.id != id);
-      this.usersListBehaviorSubject.next(listWithoutDeletedUser);
-    })
-	}
+    public deleteUser(id : Number){
+        this.http.delete(`${this.userUrl}/${id}`).subscribe({
+            next: res => {
+                const listWithoutDeletedUser = this.users.value.filter(u => u.id != id);
+                this.users.next(listWithoutDeletedUser);
+            },
+            error: err => alert(err.message)
+        });
+    }
+
 }

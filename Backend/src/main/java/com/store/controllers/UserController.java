25593f67bc91f1fb5028109e0596ed5337b87a28
vitorrf4 <vitorrf4@gmail.com>
2023-09-26 14:08:58 -0300

    change user controller to adapt to service

--- Backend/src/main/java/com/store/controllers/UserController.java
+++ Backend/src/main/java/com/store/controllers/UserController.java
@@ -1,80 +1,79 @@
 package com.store.controllers;
 
 import com.store.models.User;
-import com.store.repos.UserRepository;
+import com.store.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
-@RestController
+@RestController(value = "UserController")
 @RequestMapping(path = "/users")
 @CrossOrigin // only allow origin from deployed frontend server
 public class UserController {
-    public UserRepository repo;
+    public UserService service;
     public Logger logger = Logger.getLogger("USER_CONTROLLER_DEBUG");
 
     @Autowired
-    public UserController(UserRepository repo) {
-        this.repo = repo;
+    public UserController(UserService service) {
+        this.service = service;
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        return ResponseEntity.ok(repo.findAll());
+    public ResponseEntity<List<User>> callGetAllUsers() {
+        List<User> users = service.getAllUsers();
+
+        if (users == null) return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping(path="/{id}")
-    public ResponseEntity<?> getUserById(@PathVariable Long id) {
-        if(!repo.existsById(id))
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<?> callGetUserById(@PathVariable Long id) {
+        if (service.isIdInvalid(id)) return ResponseEntity.badRequest().build();
 
+        Optional<User> user = service.getUser(id);
 
-        return ResponseEntity.ok(repo.findById(id));
+        if (user.isEmpty()) return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(user.get());
     }
 
     @PostMapping
-    public ResponseEntity<User> addUser(@RequestBody User user) throws URISyntaxException {
-        if (user == null || user.getName().isEmpty() || user.getName().isBlank() ||
-                user.getPassword().isEmpty() || user.getPassword().isBlank())
-            return ResponseEntity.badRequest().build();
+    public ResponseEntity<User> callCreateUser(@RequestBody User user) {
+        if (service.isUserInvalid(user)) return ResponseEntity.badRequest().build();
 
-        if (repo.findByName(user.getName()) != null)
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        User savedUser = service.createUser(user);
 
+        if (savedUser == null) return ResponseEntity.status(HttpStatus.CONFLICT).build();
 
-        User createdUser = repo.save(user);
-
-        return ResponseEntity.created(new URI("/users/" + createdUser.getId())).body(createdUser); // change created uri for deployed server
-
+        return ResponseEntity.created(URI.create("/users/" + savedUser.getId())).body(savedUser); // change created uri for deployed server
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<User> changeUser(@PathVariable Long id, @RequestBody User user) {
-        if (!repo.existsById(id))
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<User> callChangeUser(@RequestBody User user) {
+        if (service.isUserInvalid(user)) return ResponseEntity.badRequest().build();
 
-        if (user.getName().isEmpty() || user.getName().isBlank() ||
-                user.getPassword().isBlank() || user.getPassword().isEmpty())
-            return ResponseEntity.badRequest().build();
+        User modifiedUser = service.changeUser(user);
 
-
-        User modifiedUser = repo.save(user);
+        if (modifiedUser == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 
         return ResponseEntity.ok(modifiedUser);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
-        if (!repo.existsById(id))
-            return ResponseEntity.notFound().build();
+        if (service.isIdInvalid(id)) return ResponseEntity.badRequest().build();
 
+        boolean isDeleted = service.deleteUser(id);
 
-        repo.deleteById(id);
+        if (!isDeleted) return ResponseEntity.notFound().build();
+
         return ResponseEntity.noContent().build();
     }
 }

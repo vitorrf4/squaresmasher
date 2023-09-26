5708a911171912554c749deeb92d94e1f65db778
vitorrf4 <vitorrf4@gmail.com>
2023-09-26 14:09:32 -0300

    update controller tests to fit to service

--- Backend/src/test/java/com/store/UserControllerTest.java
+++ Backend/src/test/java/com/store/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.store;
 import com.store.controllers.UserController;
 import com.store.models.User;
 import com.store.repos.UserRepository;
+import com.store.services.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,18 +21,14 @@ import static org.mockito.BDDMockito.given;
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
-	@MockBean private UserRepository repo;
+	@MockBean private UserService service;
 	@Autowired private UserController controller;
 
 	@Test @DisplayName("GET /users - Success")
 	public void whenGetUsers_thenSuccess() {
-		User userToAdd1 = new User();
-		User userToAdd2 = new User();
+		given(service.getAllUsers()).willReturn(new ArrayList<>(List.of(new User(), new User())));
 
-		given(repo.findAll()).willReturn(new ArrayList<>(List.of(userToAdd1, userToAdd2)));
-
-		assertThat(controller.getAllUsers().getBody().size()).isEqualTo(2);
-		assertThat(controller.getAllUsers().getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(controller.callGetAllUsers().getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 	@Test @DisplayName("GET /users/1 - Success")
@@ -39,16 +36,15 @@ class UserControllerTest {
 		User user1 = new User();
 		user1.setName("test name");
 
-		given(repo.existsById(1L)).willReturn(true);
-		given(repo.findById(1L)).willReturn(Optional.of(user1));
+		given(service.getUser(1L)).willReturn(Optional.of(user1));
 
-		assertThat(controller.getUserById(1L).getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(controller.getUserById(1L).getBody()).isEqualTo(Optional.of(user1));
+		assertThat(controller.callGetUserById(1L).getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(controller.callGetUserById(1L).getBody()).isEqualTo(user1);
 	}
 
 	@Test @DisplayName("GET /users/1 - NotFound")
 	public void whenGetUserById_thenNotFound() {
-		assertThat(controller.getUserById(1L).getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(controller.callGetUserById(1L).getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
 	@Test @DisplayName("POST /users/1 - Created")
@@ -58,29 +54,30 @@ class UserControllerTest {
 		user1.setName("test name");
 		user1.setPassword("test password");
 
-		given(repo.save(user1)).willReturn(user1);
+		given(service.createUser(user1)).willReturn(user1);
 		ResponseEntity<User> expectedEntity = ResponseEntity.created(new URI("/users/1")).body(user1);
 
-		assertThat(controller.addUser(user1).getStatusCode()).isEqualTo(HttpStatus.CREATED);
-		assertThat(controller.addUser(user1)).isEqualTo(expectedEntity);
+		assertThat(controller.callCreateUser(user1).getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		assertThat(controller.callCreateUser(user1)).isEqualTo(expectedEntity);
 	}
 
 	@Test @DisplayName("POST /users - BadRequest")
-	public void whenAddUser_thenBadRequest() throws URISyntaxException {
-		assertThat(controller.addUser(new User()).getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+	public void whenAddUser_thenBadRequest() {
+		given(service.isUserInvalid(new User())).willReturn(true);
+		assertThat(controller.callCreateUser(new User()).getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
 	}
 
 	@Test @DisplayName("POST /users - Conflict")
-	public void whenAddUser_thenConflict() throws URISyntaxException {
+	public void whenAddUser_thenConflict()  {
 		User u1 = new User();
 		u1.setName("conflict");
 		User u2 = new User();
 		u2.setName("conflict");
 		u2.setPassword("test passwor");
 
-		given(repo.findByName("conflict")).willReturn(u1);
+		given(service.getUser(1L)).willReturn(Optional.of(u1));
 
-		assertThat(controller.addUser(u2).getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
+		assertThat(controller.callCreateUser(u2).getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
 	}
 
 	@Test @DisplayName("PUT /users/1 - Success")
@@ -93,28 +90,26 @@ class UserControllerTest {
 		modifiedUser.setName("another name");
 		modifiedUser.setPassword("test password");
 
-		given(repo.existsById(1L)).willReturn(true);
-		given(repo.save(u1)).willReturn(modifiedUser);
+		given(service.changeUser(u1)).willReturn(modifiedUser);
 
-		assertThat(controller.changeUser(1L, u1).getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(controller.changeUser(1L, u1).getBody()).isEqualTo(modifiedUser);
+		assertThat(controller.callChangeUser(u1).getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(controller.callChangeUser(u1).getBody()).isEqualTo(modifiedUser);
 	}
 
 	@Test @DisplayName("PUT /users/1 - NotFound")
 	public void whenPutUser_thenNotFound() {
-		assertThat(controller.changeUser(1L, new User()).getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(controller.callChangeUser(new User()).getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
-	@Test @DisplayName("PUT /users/1 - NotFound")
+	@Test @DisplayName("PUT /users/1 - BadRequest")
 	public void whenPutUser_thenBadRequest() {
-		given(repo.existsById(1L)).willReturn(true);
-		assertThat(controller.changeUser(1L, new User()).getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+		given(service.isUserInvalid(new User())).willReturn(true);
+		assertThat(controller.callChangeUser(new User()).getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
 	}
 
 	@Test @DisplayName("DELETE /users/1 - NoContent")
 	public void whenDeleteUser_thenNoContent() {
-		given(repo.existsById(1L)).willReturn(true);
-
+		given(service.deleteUser(1L)).willReturn(true);
 		assertThat(controller.deleteUser(1L).getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
 	}
 

c8520142fc50c290ef4ac0329746bb962b5b710a
vitorrf4 <vitorrf4@gmail.com>
2023-09-26 14:08:32 -0300

    create user service

---
+++ Backend/src/main/java/com/store/services/UserService.java
@@ -0,0 +1,66 @@
+package com.store.services;
+
+import com.store.models.User;
+import com.store.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    public UserRepository repo;
+
+    @Autowired
+    public UserService(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<User> getAllUsers() {
+        if (repo == null) return null;
+
+        return repo.findAll();
+    }
+
+    public Optional<User> getUser(Long id) {
+        if (isIdInvalid(id))
+            return Optional.empty();
+
+        return repo.findById(id);
+    }
+
+    public User createUser(User user) {
+        if (isUserInvalid(user) || repo.findByName(user.getName()) != null)
+            return null;
+
+        return repo.save(user);
+    }
+
+    public User changeUser(User user) {
+        if (isUserInvalid(user) || !repo.existsById(user.getId())) return null;
+
+        return repo.save(user);
+    }
+
+    public boolean deleteUser(Long id) {
+        if (isIdInvalid(id))
+            return false;
+
+        Optional<User> userToBeDeleted = repo.findById(id);
+
+        if (userToBeDeleted.isEmpty()) return false;
+
+        repo.delete(userToBeDeleted.get());
+
+        return true;
+    }
+
+    public boolean isUserInvalid(User user) {
+        return user == null || user.getName().isEmpty() || user.getName().isBlank() ||
+                user.getPassword().isEmpty() || user.getPassword().isBlank();
+    }
+
+    public boolean isIdInvalid(Long id) {
+        return id == null || id < 1;
+    }
+}

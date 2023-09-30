fe184721db49615791400cc826c691e73eaa9c14
vitorrf4 <vitorrf4@gmail.com>
2023-09-30 16:51:07 -0300

    break down random sale service into methods

--- Backend/src/main/java/com/store/services/RandomSaleService.java
+++ Backend/src/main/java/com/store/services/RandomSaleService.java
@@ -2,57 +2,60 @@ package com.store.services;
 
 import com.store.models.*;
 import com.store.repos.StoreRepository;
-import com.store.repos.StoreStockRepository;
 import com.store.repos.UserRepository;
+import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import net.datafaker.Faker;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 @Service
 public class RandomSaleService {
-    private final Faker faker = new Faker();
-    private final StoreStockRepository stockRepository;
     private final StoreRepository storeRepository;
     private final UserRepository userRepository;
 
     @Autowired
-    public RandomSaleService(StoreStockRepository stockRepository, StoreRepository storeRepository, UserRepository userRepository) {
-        this.stockRepository = stockRepository;
+    public RandomSaleService(StoreRepository storeRepository, UserRepository userRepository) {
         this.storeRepository = storeRepository;
         this.userRepository = userRepository;
     }
 
     @Transactional
     public Sale generateSale(Long id) {
-        //TODO break down this function
-        Customer customer = new Customer(faker.name().firstName());
+        Store userStore = getUserStore(id);
+        List<MovieCopy> moviesInStock = userStore.getStock().getAllCopies();
+
+        SaleItem randomMovie = getRandomSaleItem(moviesInStock);
+
+        List<SaleItem> itemsBought = new ArrayList<>();
+        itemsBought.add(randomMovie);
+
+        Customer customer = new Customer( new Faker().name().firstName());
+        Sale sale = customer.makePurchase(itemsBought, userStore);
+        if (sale == null) return null;
+
+        userStore = storeRepository.save(userStore);
+        sale = userStore.getLastSale();
+
+        return sale;
+    }
+
+    public Store getUserStore(Long id) {
         Optional<User> user = userRepository.findById(id);
         if (user.isEmpty()) return null;
-        Store userStore = user.get().getStore();
-        List<MovieCopy> moviesInStock = stockRepository.findById(userStore.getStock().getId()).get().getAllCopies();
 
+        return storeRepository.findById(user.get().getStore().getStock().getId()).orElse(null);
+    }
+
+    public SaleItem getRandomSaleItem(List<MovieCopy> moviesInStock) {
         int randomCopyIndex = getRandomint(0, moviesInStock.size() -1);
 
         MovieCopy randomCopy = moviesInStock.get(randomCopyIndex);
         int randomCopiesAmount = getRandomint(1, randomCopy.getCopiesAmount());
 
-        SaleItem randomMovie = new SaleItem(randomCopy,  randomCopiesAmount);
-
-        List<SaleItem> items = new ArrayList<>();
-        items.add(randomMovie);
-
-        Sale sale = customer.makePurchase(items, userStore);
-        if (sale == null) return null;
-        userStore = storeRepository.save(userStore);
-        sale = userStore.getLastSale();
-
-        return sale;
+        return new SaleItem(randomCopy,  randomCopiesAmount);
     }
 
     public int getRandomint(int min, int max) {

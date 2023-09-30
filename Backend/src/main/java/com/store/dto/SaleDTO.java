1769cce3704904be60b65328eff413d76607937f
vitorrf4 <vitorrf4@gmail.com>
2023-09-30 17:00:50 -0300

    restructure SaleDTO class

--- Backend/src/main/java/com/store/dto/SaleDTO.java
+++ Backend/src/main/java/com/store/dto/SaleDTO.java
@@ -1,61 +1,12 @@
 package com.store.dto;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
-public class SaleDTO {
-    private String customerName;
-    private Map<String, Integer> moviesBought;
-    private double saleTotalPrice;
-    private LocalDateTime saleDate;
-
-    public SaleDTO(String customerName, Map<String, Integer> moviesBought, double saleTotalPrice, LocalDateTime saleDate) {
-        this.customerName = customerName;
-        this.moviesBought = moviesBought;
-        this.saleTotalPrice = saleTotalPrice;
-        this.saleDate = saleDate;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public double getSaleTotalPrice() {
-        return saleTotalPrice;
-    }
-
-    public void setSaleTotalPrice(double saleTotalPrice) {
-        this.saleTotalPrice = saleTotalPrice;
-    }
-
-    public LocalDateTime getSaleDate() {
-        return saleDate;
-    }
-
-    public void setSaleDate(LocalDateTime saleDate) {
-        this.saleDate = saleDate;
-    }
-
-    public Map<String, Integer> getMoviesBought() {
-        return moviesBought;
-    }
-
-    public void setMoviesBought(Map<String, Integer> moviesBought) {
-        this.moviesBought = moviesBought;
-    }
-
-    @Override
-    public String toString() {
-        return "SaleDTO{" +
-                "customerName='" + customerName + '\'' +
-                ", moviesBought=" + moviesBought +
-                ", saleTotalPrice=" + saleTotalPrice +
-                ", saleDate=" + saleDate +
-                '}';
-    }
-}
+public record SaleDTO (
+        String customerName,
+        String movieTitle,
+        int quantityBought,
+        double saleTotalPrice,
+        LocalDateTime saleDate
+) { }
 

a3482152f32f650bd28bc26540fe4d1f6f1f7cd7
vitorrf4 <vitorrf4@gmail.com>
2023-10-04 17:02:02 -0300

    create method to get store information

--- Backend/src/main/java/com/store/controllers/StoreController.java
+++ Backend/src/main/java/com/store/controllers/StoreController.java
@@ -7,11 +7,10 @@ import com.store.dto.StoreMapper;
 import com.store.models.Movie;
 import com.store.models.Store;
 import com.store.models.User;
-import com.store.repos.UserRepository;
+import com.store.repos.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
@@ -20,23 +19,21 @@ import java.util.logging.Logger;
 @RequestMapping(path = "/store")
 @CrossOrigin
 public class StoreController {
-    private final UserRepository userRepository;
+    private final StoreRepository storeRepository;
 
     @Autowired
-    public StoreController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public StoreController(StoreRepository storeRepository) {
+        this.storeRepository = storeRepository;
     }
 
     private final Logger logger = Logger.getLogger(this.getClass().getName());
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getStoreInformation(@PathVariable Long id) {
-        Optional<User> user = userRepository.findById(id);
-        if (user.isEmpty()) return ResponseEntity.notFound().build();
+        Optional<Store> store = storeRepository.findById(id);
+        if (store.isEmpty()) return ResponseEntity.notFound().build();
 
-        Store store = user.get().getStore();
-
-        StoreDTO storeDTO = StoreMapper.toDTO(store);
+        StoreDTO storeDTO = StoreMapper.toDTO(store.get());
 
         return ResponseEntity.ok(storeDTO);
     }
@@ -44,20 +41,20 @@ public class StoreController {
 
     @PostMapping("/{id}/restock")
     public ResponseEntity<Integer> restockMovies(@PathVariable Long id, @RequestBody List<MovieDTO> movieDTOS) {
-        Optional<User> user = userRepository.findById(id);
-        if (user.isEmpty()) return ResponseEntity.notFound().build();
+        Optional<Store> store = storeRepository.findById(id);
+        if (store.isEmpty()) return ResponseEntity.notFound().build();
 
-        int copiesInStockBefore = user.get().getStore().getStock().getCopiesTotal();
+        int copiesInStockBefore = store.get().getStock().getCopiesTotal();
 
         for (MovieDTO dto : movieDTOS) {
             logger.info("DTO: " + dto.toString());
             Movie movie = MovieMapper.toMovie(dto);
             logger.info("Movie: " + movie);
-            user.get().getStore().getStock().addMovieToStock(movie);
+            store.get().getStock().addMovieToStock(movie);
         }
 
-        userRepository.save(user.get());
-        int copiesInStockAfter = user.get().getStore().getStock().getCopiesTotal() - copiesInStockBefore;
+        storeRepository.save(store.get());
+        int copiesInStockAfter = store.get().getStock().getCopiesTotal() - copiesInStockBefore;
 
         return ResponseEntity.ok(copiesInStockAfter);
 

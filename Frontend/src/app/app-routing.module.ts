a149a3e8b96361dc2b9045c72783cc2a331e140f
vitorrf4 <vitorrf4@gmail.com>
2023-10-06 00:38:00 -0300

    redirect home path to sales

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddusersComponent } from './add-user/add-user.component';
-import { ListUsersComponent } from './list-users/list-users.component';
 import {SearchMovieComponent} from "./search-movie/search-movie.component";
 import {SaleComponent} from "./sale/sale-component";
 
 const routes: Routes = [
-  { path: "list-users", component: ListUsersComponent },
-  { path: "add-user", component: AddusersComponent },
   { path: "sales", component: SaleComponent },
-  { path: "search", component: SearchMovieComponent }
+  { path: "search", component: SearchMovieComponent },
+  { path: "**", component: SaleComponent}
 ];
 
 @NgModule({

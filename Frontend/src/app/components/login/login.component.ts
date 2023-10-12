e0ced7e9764b7f880ef680c1406deef16c945395
vitorrf4 <vitorrf4@gmail.com>
2023-10-11 23:56:38 -0300

    validate login and register form

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,5 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -8,38 +7,61 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  @ViewChild("form", {static: false}) form : any
+  @ViewChild("errorDiv") errorDiv! : ElementRef<HTMLParagraphElement>;
+  @ViewChild('errorP') errorP! : ElementRef<HTMLParagraphElement>;
   isLoginMode = true;
   loginCredentials = {name: '', password: ''};
   newUser = {storeName: '', confirm: ''};
 
-  constructor(private auth: AuthService,
-              private snackBar : MatSnackBar) {}
+  constructor(private auth: AuthService) {}
 
   onSubmit() {
-    if (this.isLoginMode)
-      this.login();
-    else
-      this.registerUser();
+		if (!this.validateLogin()) return;
 
+    if (!this.isLoginMode){
+			if (!this.validateRegister()) return;
+
+			this.registerUser();
+    }
+
+		this.auth.authenticate(this.loginCredentials);
   }
 
-  login() {
-    this.auth.authenticate(this.loginCredentials);
-    return false;
+  validateLogin(): boolean {
+    if (!this.loginCredentials.name || !this.loginCredentials.password) {
+      this.errorP.nativeElement.textContent = "All the fields must be filled";
+      this.errorDiv.nativeElement.hidden = false;
+      return false;
+    }
+
+    return true;
+  }
+
+  validateRegister() : boolean {
+		if (!this.newUser.storeName || !this.newUser.confirm) {
+			this.errorP.nativeElement.textContent = "All the fields must be filled";
+			this.errorDiv.nativeElement.hidden = false;
+			return false;
+		}
+
+    if (this.newUser.confirm != this.loginCredentials.password) {
+			this.errorP.nativeElement.textContent = "Passwords don't match";
+			this.errorDiv.nativeElement.hidden = false;
+			return false;
+    }
+
+    return true;
   }
 
   registerUser() {
-    this.auth.signUp(this.loginCredentials.name, this.loginCredentials.password, this.newUser.storeName).subscribe(() => {
-      this.login();
-    });
+    this.auth.signUp(this.loginCredentials.name, this.loginCredentials.password, this.newUser.storeName).subscribe();
   }
 
   toggleMode() {
-    this.loginCredentials.name = "";
-    this.loginCredentials.password = "";
-    this.newUser.confirm = "";
-    this.newUser.storeName = "";
+    this.loginCredentials.name = '';
+    this.loginCredentials.password = '';
+    this.newUser.confirm = '';
+    this.newUser.storeName = '';
 
     this.isLoginMode = !this.isLoginMode;
   }

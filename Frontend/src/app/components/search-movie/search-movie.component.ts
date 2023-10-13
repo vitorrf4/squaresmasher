0f60ac791e92b993b1fa5e03a357a3b2a91650ad
vitorrf4 <vitorrf4@gmail.com>
2023-10-12 21:20:51 -0300

    validate restock component

--- Frontend/src/app/components/search-movie/search-movie.component.ts
+++ Frontend/src/app/components/search-movie/search-movie.component.ts
@@ -2,9 +2,10 @@ import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {SearchService} from "../../services/search.service";
 import {Movie} from "../../models/movie";
 import {StoreService} from "../../services/store.service";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, config} from "rxjs";
 import {AuthService} from "../../services/auth.service";
 import {User} from "../../models/user";
+import {MatSnackBar, MatSnackBarConfig} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-search-movie',
@@ -19,24 +20,52 @@ export class SearchMovieComponent {
 
   constructor(private searchService: SearchService,
               private storeService: StoreService,
-              private authService: AuthService) {
+              private authService: AuthService,
+              private snackbar: MatSnackBar) {
     this.user = this.authService.userValue;
   }
 
   public searchMovie() {
+    if (!this.query) {
+      this.showErrorSnackbar("Search field cannot be empty");
+      return;
+    }
+
     this.searchService.searchMovie(this.query).subscribe({
-      next: res => this.movies = res
+      next: res => {
+        this.movies = res;
+        if (this.movies.length == 0) this.showErrorSnackbar("No movies found");
+      }
     })
+  }
+
+  showErrorSnackbar(message: string) {
+    this.snackbar.open(message, "Error" , {
+      duration: 4000,
+      panelClass: ["error"],
+      horizontalPosition: "right",
+      verticalPosition: "top"
+    });
   }
 
   public restockMovies() {
     let moviesToAdd : Movie[] = [];
 
     for (let movie of this.movies) {
+      if (movie.copiesAmount >= 10000000 || movie.copiesAmount < 0) {
+        this.showErrorSnackbar("Unavailable quantity")
+        return;
+      }
+
       if (movie.copiesAmount > 0)
         moviesToAdd.push(movie)
     }
-    //TODO validate maximum int input
+
+    if (moviesToAdd.length == 0) {
+      this.showErrorSnackbar("No copies added");
+      return;
+    }
+
 
     this.storeService.callRestockMoviesApi(moviesToAdd, this.user.id).subscribe(() => {
       this.storeService.getUpdatedStore(this.user.id);
